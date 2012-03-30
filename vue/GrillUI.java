f8b3472f6410ecf251a31744392122fb574456df
Bous <bousmina.a@gmail.com>
2012-03-30 16:04:52 +0200

    2nd commit

---
+++ vue/GrillUI.java
@@ -0,0 +1,6 @@
+package vue;
+
+public class GrillUI {
+
+}
+ 
---
+++ vue/MainWindow.java
@@ -0,0 +1,47 @@
+/**
+ * 
+ */
+package vue;
+
+import java.awt.GridLayout;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.WindowConstants;
+import javax.swing.border.EmptyBorder;
+
+/**
+ * @author bous
+ *
+ */
+public class MainWindow {
+
+	private JPanel panel;
+	private JFrame frame;
+	
+	public MainWindow()
+	{
+		frame = new JFrame("Automate cellulaire");
+		frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+		
+		panel = new JPanel();		
+		panel.setLayout(new GridLayout(3,2,10,10));
+		panel.setBorder(new EmptyBorder(30,30,30,30));
+		panel.setSize(640, 480);
+		JLabel label = new JLabel("automate celullaire");
+		panel.add(label);
+		frame.add(panel,"Center");
+		
+		frame.pack();
+		frame.setVisible(true);
+	}
+	
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		MainWindow mw = new MainWindow();
+	}
+
+}
