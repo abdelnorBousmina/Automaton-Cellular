f4bfaf0344aea8d63e2cec8b6d42fbf683de9f8f
Albin Poignot <mithrop@gmail.com>
2012-03-30 18:11:19 +0200

    Update .gitignore + création classe Neighborhood + première implémentation du modèle + création classe Person

--- model/MathModel.java
+++ model/MathModel.java
@@ -1,5 +1,64 @@
 package model;
 
-public class MathModel {
 
+public class MathModel {
+	
+	/**
+	 * Probabilité de panique des personnes dans le système
+	 */
+	private static Float panic = 0.05f;
+
+	/**
+	 * Définit la prochaine position de la case centrale du voisinage
+	 * @param voisinage Le voisinage de la personne qui doit se déplacer
+	 * @return Un tableau de la forme :
+	 * 		tab[0] la valeur à ajouter à x pour atteindre la prochaine position
+	 *		tab[1] la valeur à ajouter à y pour atteindre la prochaine position
+	 */
+	public Integer[] bouger(Neighborhood voisinage)
+	{
+		Integer nextX = 0, nextY = 0, ligne, colonne;
+		Integer[] retour = new Integer[2];
+		
+		if(Math.random() >= panic)
+		{
+			// Parcours du voisinage
+			for (ligne = 0; ligne < voisinage.getNbLignes(); ligne++) 
+			{
+				
+				for (colonne = 0; colonne < voisinage.getNbColonnes(); colonne++) 
+				{
+					// Si la case évaluée est plus proche de la sortie que la destination
+					// déjà choisie, la prochaine case est la case évaluée
+					// En cas d'égalité, un choix aléatoire est effectué
+					if(voisinage.getValue(ligne, colonne) < voisinage.getValue(nextX, nextY))
+					{
+						nextX = ligne;
+						nextY = colonne;
+					}
+					else if(voisinage.getValue(ligne, colonne) == voisinage.getValue(nextX, nextY))
+					{
+						// 1 chance sur 2 d'aller finalement en (ligne, colonne)
+						if(Math.random() <= 0.50f)
+						{
+							nextX = ligne;
+							nextY = colonne;
+						}
+					} // Fin détermination next	
+				
+				}// Fin parcours des colonnes
+				
+			} // Fin du parcours des lignes
+		
+		}
+		else
+		{
+			// TODO Implémenter le comportement de panique 
+		}
+		
+		retour[0] = nextX - 1;
+		retour[1] = nextY - 1;
+		return retour;
+		
+	}
 }
---
+++ model/Person.java
@@ -0,0 +1,61 @@
+/**
+ * 
+ */
+package model;
+
+import vue.PersonUI;
+
+/**
+ * @author albin
+ *
+ */
+public class Person {
+	
+	private PersonUI ui;
+	private Integer x;
+	private Integer y;
+	
+	
+	public Person()
+	{
+		// TODO Coder le constructeur
+	}
+	
+	/**
+	 * @return the ui
+	 */
+	public PersonUI getUi() {
+		return ui;
+	}
+	/**
+	 * @param ui the ui to set
+	 */
+	public void setUi(PersonUI ui) {
+		this.ui = ui;
+	}
+	/**
+	 * @return the x
+	 */
+	public Integer getX() {
+		return x;
+	}
+	/**
+	 * @param x the x to set
+	 */
+	public void setX(Integer x) {
+		this.x = x;
+	}
+	/**
+	 * @return the y
+	 */
+	public Integer getY() {
+		return y;
+	}
+	/**
+	 * @param y the y to set
+	 */
+	public void setY(Integer y) {
+		this.y = y;
+	}
+
+}
